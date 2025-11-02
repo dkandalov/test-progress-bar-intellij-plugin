6288b756a71354132b9386815124a3db02622db6
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-11-02 13:58:58 +0000

    Add Java tests project

---
+++ java-tests/src/test/java/com/example/TrivialTests.java
@@ -0,0 +1,317 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TrivialTests {
+    private static void someAssertion() {
+        //noinspection EqualsWithItself
+        assertEquals(1, 1);
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException ignored) {
+        }
+    }
+
+    @Test public void test1() {
+        someAssertion();
+    }
+
+    @Test public void test2() {
+        someAssertion();
+    }
+    @Test public void test3() {
+        someAssertion();
+    }
+    @Test public void test4() {
+        someAssertion();
+    }
+    @Test public void test5() {
+        someAssertion();
+    }
+    @Test public void test6() {
+        someAssertion();
+    }
+    @Test public void test7() {
+        someAssertion();
+    }
+    @Test public void test8() {
+        someAssertion();
+    }
+    @Test public void test9() {
+        someAssertion();
+    }
+    @Test public void test10() {
+        someAssertion();
+    }
+    @Test public void test11() {
+        someAssertion();
+    }
+    @Test public void test12() {
+        someAssertion();
+    }
+    @Test public void test13() {
+        someAssertion();
+    }
+    @Test public void test14() {
+        someAssertion();
+    }
+    @Test public void test15() {
+        someAssertion();
+    }
+    @Test public void test16() {
+        someAssertion();
+    }
+    @Test public void test17() {
+        someAssertion();
+    }
+    @Test public void test18() {
+        someAssertion();
+    }
+    @Test public void test19() {
+        someAssertion();
+    }
+    @Test public void test20() {
+        someAssertion();
+    }
+    @Test public void test21() {
+        someAssertion();
+    }
+    @Test public void test22() {
+        someAssertion();
+    }
+    @Test public void test23() {
+        someAssertion();
+    }
+    @Test public void test24() {
+        someAssertion();
+    }
+    @Test public void test25() {
+        someAssertion();
+    }
+    @Test public void test26() {
+        someAssertion();
+    }
+    @Test public void test27() {
+        someAssertion();
+    }
+    @Test public void test28() {
+        someAssertion();
+    }
+    @Test public void test29() {
+        someAssertion();
+    }
+    @Test public void test30() {
+        someAssertion();
+    }
+    @Test public void test31() {
+        someAssertion();
+    }
+    @Test public void test32() {
+        someAssertion();
+    }
+    @Test public void test33() {
+        someAssertion();
+    }
+    @Test public void test34() {
+        someAssertion();
+    }
+    @Test public void test35() {
+        someAssertion();
+    }
+    @Test public void test36() {
+        someAssertion();
+    }
+    @Test public void test37() {
+        someAssertion();
+    }
+    @Test public void test38() {
+        someAssertion();
+    }
+    @Test public void test39() {
+        someAssertion();
+    }
+    @Test public void test40() {
+        someAssertion();
+    }
+    @Test public void test41() {
+        someAssertion();
+    }
+    @Test public void test42() {
+        someAssertion();
+    }
+    @Test public void test43() {
+        someAssertion();
+    }
+    @Test public void test44() {
+        someAssertion();
+    }
+    @Test public void test45() {
+        someAssertion();
+    }
+    @Test public void test46() {
+        someAssertion();
+    }
+    @Test public void test47() {
+        someAssertion();
+    }
+    @Test public void test48() {
+        someAssertion();
+    }
+    @Test public void test49() {
+        someAssertion();
+    }
+    @Test public void test50() {
+        someAssertion();
+    }
+    @Test public void test51() {
+        someAssertion();
+    }
+    @Test public void test52() {
+        someAssertion();
+    }
+    @Test public void test53() {
+        someAssertion();
+    }
+    @Test public void test54() {
+        someAssertion();
+    }
+    @Test public void test55() {
+        someAssertion();
+    }
+    @Test public void test56() {
+        someAssertion();
+    }
+    @Test public void test57() {
+        someAssertion();
+    }
+    @Test public void test58() {
+        someAssertion();
+    }
+    @Test public void test59() {
+        someAssertion();
+    }
+    @Test public void test60() {
+        someAssertion();
+    }
+    @Test public void test61() {
+        someAssertion();
+    }
+    @Test public void test62() {
+        someAssertion();
+    }
+    @Test public void test63() {
+        someAssertion();
+    }
+    @Test public void test64() {
+        someAssertion();
+    }
+    @Test public void test65() {
+        someAssertion();
+    }
+    @Test public void test66() {
+        someAssertion();
+    }
+    @Test public void test67() {
+        someAssertion();
+    }
+    @Test public void test68() {
+        someAssertion();
+    }
+    @Test public void test69() {
+        someAssertion();
+    }
+    @Test public void test70() {
+        someAssertion();
+    }
+    @Test public void test71() {
+        someAssertion();
+    }
+    @Test public void test72() {
+        someAssertion();
+    }
+    @Test public void test73() {
+        someAssertion();
+    }
+    @Test public void test74() {
+        someAssertion();
+    }
+    @Test public void test75() {
+        someAssertion();
+    }
+    @Test public void test76() {
+        someAssertion();
+    }
+    @Test public void test77() {
+        someAssertion();
+    }
+    @Test public void test78() {
+        someAssertion();
+    }
+    @Test public void test79() {
+        someAssertion();
+    }
+    @Test public void test80() {
+        someAssertion();
+    }
+    @Test public void test81() {
+        someAssertion();
+    }
+    @Test public void test82() {
+        someAssertion();
+    }
+    @Test public void test83() {
+        someAssertion();
+    }
+    @Test public void test84() {
+        someAssertion();
+    }
+    @Test public void test85() {
+        someAssertion();
+    }
+    @Test public void test86() {
+        someAssertion();
+    }
+    @Test public void test87() {
+        someAssertion();
+    }
+    @Test public void test88() {
+        someAssertion();
+    }
+    @Test public void test89() {
+        someAssertion();
+    }
+    @Test public void test90() {
+        someAssertion();
+    }
+    @Test public void test91() {
+        someAssertion();
+    }
+    @Test public void test92() {
+        someAssertion();
+    }
+    @Test public void test93() {
+        someAssertion();
+    }
+    @Test public void test94() {
+        someAssertion();
+    }
+    @Test public void test95() {
+        someAssertion();
+    }
+    @Test public void test96() {
+        someAssertion();
+    }
+    @Test public void test97() {
+        someAssertion();
+    }
+    @Test public void test98() {
+        someAssertion();
+    }
+    @Test public void test99() {
+        someAssertion();
+    }
+    @Test public void test100() {
+        someAssertion();
+    }
+}
